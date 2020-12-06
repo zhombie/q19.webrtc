534bd9b95b543e8de9d635a67e4c14d590dcd557
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2020-12-07 04:09:34 +0500

    Fixed import error

--- webrtc/src/main/java/kz/q19/webrtc/SetupParams.kt
+++ webrtc/src/main/java/kz/q19/webrtc/SetupParams.kt
@@ -1,12 +1,12 @@
 package kz.q19.webrtc
 
-import kz.q19.domain.model.webrtc.WebRTCIceServer
+import kz.q19.domain.model.webrtc.IceServer
 
 data class SetupParams(
     var isLocalAudioEnabled: Boolean = Configs.LOCAL_AUDIO_ENABLED,
     var isLocalVideoEnabled: Boolean = Configs.LOCAL_VIDEO_ENABLED,
     var isRemoteAudioEnabled: Boolean = Configs.REMOTE_AUDIO_ENABLED,
     var isRemoteVideoEnabled: Boolean = Configs.REMOTE_VIDEO_ENABLED,
-    var iceServers: List<WebRTCIceServer> = emptyList(),
+    var iceServers: List<IceServer> = emptyList(),
     var videoCodecHwAcceleration: Boolean = true
 )
